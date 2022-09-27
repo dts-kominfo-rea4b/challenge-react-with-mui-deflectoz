ad18603f8c65e8dfc9292c959cfea6a0ff0dd88b
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2022-09-27 23:01:15 +0700

    Challenge with MUI

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,36 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
 import React from 'react';
+import { yellow } from "@mui/material/colors";
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+        <>
+        <List sx={{bgcolor: yellow[50]}}>
+   <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+        <Avatar src={data.photo}></Avatar>
+    </ListItemAvatar>
+    <ListItemText
+				primary={data.name}
+				secondaryTypographyProps={{ component: "div" }}
+				secondary={
+					<React.Fragment>
+						<Typography>{data.phone}</Typography>
+						<Typography>{data.email}</Typography>
+					</React.Fragment>
+				}
+				title={data.name}
+			/>
+   </ListItem>
+   </List>
+   </>
+        );
 };
 
 export default Contact;
